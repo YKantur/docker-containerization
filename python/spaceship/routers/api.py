d08cdec21a3179560e24ca733db389200419e61f
YKantur <kntryarik@gmail.com>
2025-06-02 14:58:57 +0300

    Import numpy-library to check how it impacts on docker image

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -1,3 +1,4 @@
+import numpy as np
 from fastapi import APIRouter
 
 router = APIRouter()
@@ -5,3 +6,14 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get("/matrix-mult")
+async def matrix_mult():
+    a = np.random.rand(10, 10)
+    b = np.random.rand(10, 10)
+    product = a.dot(b)
+    return {
+        "matrix_a": a.tolist(),
+        "matrix_b": b.tolist(),
+        "product": product.tolist(),
+    }
